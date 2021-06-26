b08e29ef86850a203be886f9e01a85aeb71565bf
lighthouse-dev <lighthouse2.dev@gmail.com>
2021-06-27 03:39:29 +0900

    Default Port 변경

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const app = express();
 // 보안을 위해 X-Powered-By 헤더를 제거 (https://expressjs.com/ko/advanced/best-practice-security.html)
 app.disable('x-powered-by');
 
-const port = Number(process.env.DUMMY_PLUG_PORT) || 8081;
+const port = Number(process.env.DUMMY_PLUG_PORT) || 8080;
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`🚀 start dummy-plug. http://localhost:${port}/`);
